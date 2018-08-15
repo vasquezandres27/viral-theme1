2fb58de0949658e31bfb764fd80e238059831556
vasquezandres27 <42306350+vasquezandres27@users.noreply.github.com>
2018-08-15 16:32:23 -0600

    Add files via upload

---
+++ viral-tema/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Viral</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+</head>
+<style>
+    .container{
+        background-color: #FFFFFF;
+        text-align: center;
+        font-family: Tahoma;
+    } 
+    .principal{
+        background-color: #FFFFFF;
+        text-align: left;
+    } 
+    .sidebar{
+        background-color: #FFFFFF;
+    } 
+    .footer{
+        background-color: #000000;
+        text-align: center;
+        color: #FFFFFF;
+    }
+</style>
+<body>
+</div>
+ <div class="container">
+    <div class="row"> 
+        <div class="header col-6">
+        <img src="viral.png" class="img-fluid" alt="Viral Logo">
+        </div>
+        
+        <div class="header col">
+        </div>
+        <i class="fab fa-facebook"></i><a href="https://facebook.com"></a>
+        <i class="fab fa-instagram"></i><a href="https://instagram.com"></a>
+        <i class="fab fa-twitter-square"></i><a href="https://twitter"></a>
+        <i class="fab fa-youtube-square"></i><a href="https://youtube.com"></a>
+
+        <div class="header col">
+        <button type="button" class="btn btn-outline-dark">Buscar aqui</button>
+        <i class="fas fa-search"></i>
+        </div>
+        <div class="w-100"></div>
+        <div class="header col-6">
+        Tecnología YouTube Música Cine Series
+        </div>
+        
+    </div>
+ </div>
+ <div class="container">
+     <div class="row"> 
+        <div class="principal col-8">
+            
+        </div>
+        <div class="principal col-8">
+            Tecnología
+        </div>
+        <div class="sidebar col-4">
+            Publicidad
+    </div>
+  </div>
+<div class="container"> 
+    <div class="row"> 
+        <div class="footer col-12">
+        <img src="viral2.png" class="img-fluid" alt="Viral Logo 2"> 
+        </div>
+    </div>
+</div>  
+</body>
+</html>
